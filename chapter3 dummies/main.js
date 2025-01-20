8459a1782db94231b2ef8fe1811ab2ce567135f6
cadee247 <rousseaucadee@gmail.com>
2025-01-20 21:36:33 +0200

    Add files via upload

---
+++ chapter3 dummies/main.js
@@ -0,0 +1,12 @@
+var name;
+
+name = "Cadee";
+
+console.log(name.length);
+console.log(name.indexOf("v"));
+
+var question = prompt("Is this your name?" + name);
+
+
+
+document.write(" Welcome to the document " + name);
