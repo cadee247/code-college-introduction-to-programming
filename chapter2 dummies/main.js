6205f14bbcb19e9aeceb3deeb6adc24778ed47ee
cadee247 <rousseaucadee@gmail.com>
2025-01-20 21:36:05 +0200

    Add files via upload

---
+++ chapter2 dummies/main.js
@@ -0,0 +1,11 @@
+//alert("Good day World");
+
+for (var i = 0; i < 300; i++) {
+    document.write("Coding is fun!");
+};
+
+console.log('Cadee said, "Hi class, " and then we carried on with her day.');
+    
+
+var string1 = "string";
+var string2 = "string";
