275be14cbe8ad26d94f07fa5701d2783ff23d360
cadee247 <rousseaucadee@gmail.com>
2025-01-22 20:29:33 +0200

    Add files via upload

---
+++ chapter11/main.js
@@ -0,0 +1,16 @@
+var parents = ["Nicky", "Brian"];
+var friends = ["Ashleigh", "Jesse"];
+var family = parents.concat(friends).join(" ");
+var familia = document.getElementById("myFamily");
+let longArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+
+
+
+
+familia.innerHTML = "<h1>This is my family: "+ family + "</h1>";
+
+
+
